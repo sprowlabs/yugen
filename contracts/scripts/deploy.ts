af425cd845924b083b7ff7a90196e77c7e67218b
DecentralisedTech <contact@decentralised.tech>
2022-07-06 15:49:03 +1200

    Add some of the account SBT contract

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -3,23 +3,17 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { run, ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  await run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const AccountSBT = await ethers.getContractFactory("AccountSBT");
+  const accountSBT = await AccountSBT.deploy();
 
-  await greeter.deployed();
+  await accountSBT.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("AccountSBT deployed to:", accountSBT.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
